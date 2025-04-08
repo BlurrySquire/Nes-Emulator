63c9986457053ca7fc73f1f15b04e1eb4b37259b
Riley Hawksworth <114335081+BlurrySquire@users.noreply.github.com>
2025-04-08 10:17:29 +0100

    Separate addressing modes into own functions.

--- source/cpu.c
+++ source/cpu.c
@@ -2,19 +2,8 @@
 
 #include "memory_bus.h"
 
-typedef enum addressing_mode {
-	IMPLIED,
-	ACCUMULATOR,
-	IMMEDIATE,
-	ZEROPAGE, ZEROPAGEX, ZEROPAGEY,
-	RELATIVE,
-	ABSOLUTE, ABSOLUTEX, ABSOLUTEY,
-	INDIRECT, INDEXEDINDIRECT, INDIRECTINDEXED
-} addressing_mode;
-
-void opcode_nop(cpu* state, addressing_mode mode);
-
-void opcode_lda(cpu* state, addressing_mode mode);
+void opcode_nop(cpu* state);
+void opcode_lda(cpu* state, u8 operand);
 
 void cpu_init(cpu* state) {
 	state->total_cycles = 0;
@@ -39,118 +28,134 @@ void cpu_init(cpu* state) {
 void cpu_execute_instruction(cpu* state) {
 	u8 instruction = memory_read(state->program_counter);
 	state->program_counter++;
+	state->current_instruction_cycles += 1;
 
 	switch (instruction) {
-		case 0xEA: opcode_nop(state, IMPLIED); break;
+		case 0xEA: opcode_nop(state); break;
 
-		// LDA - Load accumulator
-		case 0xA9: opcode_lda(state, IMMEDIATE); break;
-		case 0xA5: opcode_lda(state, ZEROPAGE); break;
-		case 0xB5: opcode_lda(state, ZEROPAGEX); break;
-		case 0xAD: opcode_lda(state, ABSOLUTE); break;
-		case 0xBD: opcode_lda(state, ABSOLUTEX); break;
-		case 0xB9: opcode_lda(state, ABSOLUTEY); break;
-		case 0xA1: opcode_lda(state, INDEXEDINDIRECT); break;
-		case 0xB1: opcode_lda(state, INDIRECTINDEXED); break;
+		case 0xA9: opcode_lda(state, addressing_immediate(state)); break;
+		case 0xA5: opcode_lda(state, addressing_zeropage(state)); break;
+		case 0xB5: opcode_lda(state, addressing_zeropagex(state)); break;
+		case 0xAD: opcode_lda(state, addressing_absolute(state)); break;
+		case 0xBD: opcode_lda(state, addressing_absolutex(state)); break;
+		case 0xB9: opcode_lda(state, addressing_absolutey(state)); break;
+		case 0xA1: opcode_lda(state, addressing_indexedindirect(state)); break;
+		case 0xB1: opcode_lda(state, addressing_indirectindexed(state)); break;
 	}
 
 	state->total_cycles += state->current_instruction_cycles;
 }
 
-void opcode_nop(cpu* state, addressing_mode mode) {
-	switch (mode) {
-		case IMPLIED: {
-			state->current_instruction_cycles = 2;
-		} break;
-	}
-
-	state->current_instruction_cycles = 2;
+void opcode_nop(cpu* state) {
+	state->current_instruction_cycles += 1;
 }
 
-void opcode_lda(cpu* state, addressing_mode mode) {
-	switch (mode) {
-		case IMMEDIATE: {
-			state->accumulator = memory_read(state->program_counter);
-			state->program_counter++;
-
-			state->current_instruction_cycles = 2;
-		} break;
-
-		case ZEROPAGE: {
-			u8 address = memory_read(state->program_counter);
-			state->accumulator = memory_read(address);
-			state->program_counter++;
-
-			state->current_instruction_cycles = 3;
-		} break;
-
-		case ZEROPAGEX: {
-			u8 address = memory_read(state->program_counter) + state->register_x;
-			state->accumulator = memory_read(address);
-			state->program_counter++;
-
-			state->current_instruction_cycles = 4;
-		} break;
-
-		case ABSOLUTE: {
-			u16 address = memory_read(state->program_counter) | (memory_read(state->program_counter + 1) << 8);
-			state->program_counter += 2;
-			state->accumulator = memory_read(address);
-
-			state->current_instruction_cycles = 4;
-		} break;
-
-		case ABSOLUTEX: {
-			u16 base = memory_read(state->program_counter) | (memory_read(state->program_counter + 1) << 8);
-			u16 address = base + state->register_x;
-			state->program_counter += 2;
-
-			state->accumulator = memory_read(address);
-
-			state->current_instruction_cycles = 4;
-			if ((base & 0xFF00) != (address & 0xFF00)) {
-				state->current_instruction_cycles++; // Extra cycle if crossing a page boundary
-			}
-		} break;
-
-		case ABSOLUTEY: {
-			u16 base = memory_read(state->program_counter) | (memory_read(state->program_counter + 1) << 8);
-			u16 address = base + state->register_y;
-			state->program_counter += 2;
-
-			state->accumulator = memory_read(address);
-
-			state->current_instruction_cycles = 4;
-			if ((base & 0xFF00) != (address & 0xFF00)) {
-				state->current_instruction_cycles++; // Extra cycle if crossing a page boundary
-			}
-		} break;
-
-		case INDEXEDINDIRECT: {
-			u8 pointer = memory_read(state->program_counter) + state->register_x;
-			state->program_counter++;
-
-			u8 address = memory_read(pointer) | (memory_read(pointer + 1) << 8);
-			state->accumulator = memory_read(address);
-
-			state->current_instruction_cycles = 6;
-		} break;
-
-		case INDIRECTINDEXED: {
-			u8 pointer = memory_read(state->program_counter);
-			state->program_counter++;
-
-			u16 base = memory_read(pointer) | (memory_read(pointer + 1) << 8);
-			u16 address = state->register_y;
-			state->accumulator = memory_read(address);
-
-			state->current_instruction_cycles = 5;
-			if ((base & 0xFF00) != (address & 0xFF00)) {
-				state->current_instruction_cycles++;
-			}
-		} break;
-	}
+void opcode_lda(cpu* state, u8 operand) {
+	state->accumulator = operand;
 
 	state->register_status.zero_flag = !state->accumulator;
 	state->register_status.negative_flag = state->accumulator & (1 << 7);
+}
+
+u8 addressing_immediate(cpu* state) {
+	u8 byte = memory_read(state->program_counter);
+	state->program_counter++;
+	state->current_instruction_cycles += 1;
+	
+	return byte;
+}
+
+u8 addressing_zeropage(cpu* state) {
+	u8 address = memory_read(state->program_counter);
+	state->program_counter++;
+
+	u8 byte = memory_read(address);
+	state->current_instruction_cycles += 2;
+
+	return byte;
+}
+
+u8 addressing_zeropagex(cpu* state) {
+	u8 address = memory_read(state->program_counter) + state->register_x;
+	state->program_counter++;
+
+	u8 byte = memory_read(address);
+	state->current_instruction_cycles += 3;
+
+	return byte;
+}
+
+u8 addressing_zeropagey(cpu* state) {
+	u8 address = memory_read(state->program_counter) + state->register_y;
+	state->program_counter++;
+
+	u8 byte = memory_read(address);
+	state->current_instruction_cycles += 3;
+
+	return byte;
+}
+
+u8 addressing_absolute(cpu* state) {
+	u16 address = memory_read(state->program_counter) | (memory_read(state->program_counter + 1) << 8);
+	state->program_counter += 2;
+	
+	u8 byte = memory_read(address);
+	state->current_instruction_cycles += 3;
+
+	return byte;
+}
+
+u8 addressing_absolutex(cpu* state) {
+	u16 base = memory_read(state->program_counter) | (memory_read(state->program_counter + 1) << 8);
+	u16 address = base + state->register_x;
+	state->program_counter += 2;
+	
+	u8 byte = memory_read(address);
+	state->current_instruction_cycles += 3;
+	if ((base & 0xFF00) != (address & 0xFF00)) {
+		state->current_instruction_cycles++;
+	}
+
+	return byte;
+}
+
+u8 addressing_absolutey(cpu* state) {
+	u16 base = memory_read(state->program_counter) | (memory_read(state->program_counter + 1) << 8);
+	u16 address = base + state->register_y;
+	state->program_counter += 2;
+	
+	u8 byte = memory_read(address);
+	state->current_instruction_cycles += 3;
+	if ((base & 0xFF00) != (address & 0xFF00)) {
+		state->current_instruction_cycles++;
+	}
+
+	return byte;
+}
+
+u8 addressing_indexedindirect(cpu* state) {
+	u8 pointer = memory_read(state->program_counter) + state->register_x;
+	state->program_counter++;
+
+	u8 address = memory_read(pointer) | (memory_read(pointer + 1) << 8);
+	u8 byte = memory_read(address);
+	state->current_instruction_cycles += 5;
+
+	return byte;
+}
+
+u8 addressing_indirectindexed(cpu* state) {
+	u8 pointer = memory_read(state->program_counter);
+	state->program_counter++;
+
+	u16 base = memory_read(pointer) | (memory_read(pointer + 1) << 8);
+	u16 address = base + state->register_y;
+	u8 byte = memory_read(address);
+
+	state->current_instruction_cycles += 4;
+	if ((base & 0xFF00) != (address & 0xFF00)) {
+		state->current_instruction_cycles++;
+	}
+
+	return byte;
 }
